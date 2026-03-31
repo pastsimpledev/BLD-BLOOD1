0e35003bb7d7aa2a5862a605c9cd6d51638b2d09
BLOOD212 <g95829914@gmail.com>
2026-03-31 02:21:36 +0200

    Update brasiliana.js

--- plugins/brasiliana.js
+++ plugins/brasiliana.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-  const message = `*QUESTO COMANDO RICORDA AD ASIA DI FAR CONOSCERE A ME E BOB LA BRASILIANA E CHE GLIELO SBATTIAMO OVUNQUE ALLA NOSTRA MAMACITAN
-DEVE GRUDARE OIA PAPIII MAS FORTE*.`;
+  const message = `*QUESTO COMANDO RICORDA AD ASIA DI FAR CONOSCERE A ME E BOB LA BRASILIANA E CHE GLIELO SBATTIAMO OVUNQUE ALLA NOSTRA MAMACITA DEVE GRIDARE OIA PAPIII MAS FORTE (ho già il cazzo di fuori al pensiero, bob si sta segando)*.`;
   // manda il messaggio nella chat dove il comando è stato usato, citandolo
   await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 };
