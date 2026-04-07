18fb3031ce05dfe5b275aa23693643a15cd34261
BLOOD212 <g95829914@gmail.com>
2026-04-07 16:58:54 +0200

    Update anti-bot.js

--- plugins/anti-bot.js
+++ plugins/anti-bot.js
@@ -1,4 +1,4 @@
-let whitelist = [];
+let puliti = [];
 
 function rilevaDispositivoCheck(msgID = '') {
   if (!msgID) return 'sconosciuto';
@@ -13,30 +13,22 @@ function rilevaDispositivoCheck(msgID = '') {
   return 'sconosciuto';
 }
 
-export async function before(m, { conn }) {
+export async function before(m, { conn, isAdmin, isOwner, isSam }) {
   const chat = global.db.data.chats[m.chat];
   if (!chat?.antiBot) return;
   if (!m.isGroup || !m.sender || !m.key?.id) return;
+  if (isAdmin || isOwner || isSam || m.fromMe) return;
   const msgID = m.key?.id;
   const device = rilevaDispositivoCheck(msgID);
   const sospettiDispositivi = ['bot', 'web', 'webbot'];
   if (!sospettiDispositivi.includes(device)) return;
   const metadata = await conn.groupMetadata(m.chat);
   const botNumber = conn.user.jid;
-  const autorizzati = [botNumber, metadata.owner, ...whitelist];
+  const autorizzati = [botNumber, metadata.owner, ...puliti];
   if (autorizzati.includes(m.sender)) return;
-  const currentAdmins = metadata.participants.filter(p => p.admin).map(p => p.id);
-  const èAdmin = currentAdmins.includes(m.sender);
-  if (èAdmin) {
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'demote');
-    await conn.sendMessage(m.chat, {
-      text: `> 𝐑𝐈𝐋𝐄𝐕𝐀𝐓𝐎 𝐁𝐎𝐓 ⚠️\n @${m.sender.split('@')[0]} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨 𝐫𝐞𝐭𝐫𝐨𝐜𝐞𝐬𝐬𝐨.\n𝑫𝒊𝒔𝒑𝒐𝒔𝒊𝒕𝒊𝒗𝒐: *${device.toUpperCase()}*`,
-      mentions: [m.sender]
-    });
-  }
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
   await conn.sendMessage(m.chat, {
-    text: `> 𝐑𝐈𝐋𝐄𝐕𝐀𝐓𝐎 𝐁𝐎𝐓 ⚠️\n@${m.sender.split('@')[0]} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨 𝐫𝐢𝐦𝐨𝐬𝐬𝐨.\n𝑫𝒊𝒔𝒑𝒐𝒔𝒊𝒕𝒊𝒗𝒐: *${device.toUpperCase()}*`,
+    text: `『 🚫 』 \`Bot rilevato\`\n\n➤ \`Utente:\` @${m.sender.split('@')[0]}\n➤ \`Azione:\` Rimosso dal gruppo\n➤ \`Dispositivo:\` ${device.toUpperCase()}\n\n\`BloodBot\``,
     mentions: [m.sender]
   });
 }
