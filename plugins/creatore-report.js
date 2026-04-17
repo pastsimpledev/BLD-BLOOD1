69db79d9459a7c1fb9d2ace1a15da99285379ab0
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-17 21:45:26 +0200

    Update creatore-report.js

--- plugins/creatore-report.js
+++ plugins/creatore-report.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 ┃ *Messaggio:* 
 ┃ ${text}
 ╰━━━•❃°•°❀°•°❃•━━━╯`
-    const ownerNumber = '393701330693@s.whatsapp.net' // Numero diretto dell'owner
+    const ownerNumber = '393455383567@s.whatsapp.net' // Numero diretto dell'owner
     
     await conn.sendMessage(ownerNumber, {
         text: teks,
