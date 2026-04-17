d3de63ccb70547d88539a0375cf0081da6f39a0a
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-17 22:40:44 +0200

    Update creatore-nuke.js

--- plugins/creatore-nuke.js
+++ plugins/creatore-nuke.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['stalks'];
+handler.command = ['cry'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
